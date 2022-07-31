d7c3b9fcf0b473674f890568869e795814f3625f
Arseniy <shandropolnew@gmail.com>
2022-07-31 18:35:05 +0300

    Initialize

---
+++ catalog/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path, include, re_path
+from . import views
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'), # "pk" because of class expectation
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'), # "pk" because of class expectation
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    re_path(r'^allbooks/$', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
+]
+urlpatterns += [
+    re_path(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    re_path(r'^author/create/$', views.AuthorCreate.as_view(), name='author_create'),
+    re_path(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    re_path(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
+]
+
+urlpatterns += [
+    re_path(r'^author/(?P<pk>\d+)/create_book/$', views.BookCreate.as_view(), name='author_book_create'),
+    re_path(r'^book/add/$', views.BookCreateSimple.as_view(), name='book_create'),
+
+    re_path(r'^book/(?P<pk>\d+)/update/$', views.BookUpdate.as_view(), name='book_update'),
+    re_path(r'^book/(?P<pk>\d+)/delete/$', views.BookDelete.as_view(), name='book_delete'),
+]
